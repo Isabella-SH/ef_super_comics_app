8a2d6191c0b01bbaea9438efc2ad69bc23cc6a6c
Isabella <sorianoisabella28@gmail.com>
2023-12-02 22:54:33 -0500

    implementar comics_favorites, uso el widget de item que estaba en comics_by_name

--- lib/screens/comics_favorites.dart
+++ lib/screens/comics_favorites.dart
@@ -1,5 +1,9 @@
 
+import 'package:ef_super_comics_app/models/comic.dart';
+import 'package:ef_super_comics_app/repositories/comic_favorite_repository.dart';
+import 'package:ef_super_comics_app/widgets/comics_by_name.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class ComicsFavorites extends StatefulWidget {
   const ComicsFavorites({super.key});
@@ -9,8 +13,36 @@ class ComicsFavorites extends StatefulWidget {
 }
 
 class _ComicsFavoritesState extends State<ComicsFavorites> {
+
+  ComicFavoriteRepository? _comicFavoriteRepository;
+  List<Comic>? _comics;
+
+  initialize() async{ // "?? []" -> si es nulo, devuelve un arreglo vacio
+    _comics=await _comicFavoriteRepository?.getAll()??[];
+    setState((){
+      _comics=_comics;
+    });
+  }
+
+  @override
+  void initState(){
+    _comicFavoriteRepository=ComicFavoriteRepository();
+    initialize();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text("favorites");
+
+    return Scaffold(
+        body: ListView.builder(
+        itemCount: _comics?.length ?? 0,
+        itemBuilder: ((context, index) {
+      final comic = _comics![index];
+      return ComicItem(comic: comic);
+           }
+         )
+        )
+    );
   }
 }
