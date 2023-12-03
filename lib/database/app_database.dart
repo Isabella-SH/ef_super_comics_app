63cfe753cc13ec2ac68beeb1deac0ef05d223d61
Isabella <sorianoisabella28@gmail.com>
2023-12-02 22:25:44 -0500

    impelmentar app_database.dart

---
+++ lib/database/app_database.dart
@@ -0,0 +1,22 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class PokemonDatabase{
+
+  final int version=1;
+  final String databaseName="SuperComic.db";
+  final String tableName="comics_favorites";
+
+  Database? db;
+
+  //funcion patra la creacion de nuetsra base de datos
+  //y la tabla llamada pokemons que almacena el id
+  Future<Database> openDb()async{
+    db ??= await openDatabase(join(await getDatabasesPath(), databaseName),
+        onCreate: (database, version){
+          database.execute("create table $tableName (id text primary key, name text, gender text, intelligence text, image text)");
+        }, version: version
+    );
+    return db as Database;
+  }
+}
